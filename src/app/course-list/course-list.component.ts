35536f76dbf3f843dcf94bd36806c2b5ad07dd6e
diaba <hwar@cincinnatistate.edu>
2023-10-19 16:54:19 -0400

    course

---
+++ src/app/course-list/course-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css'],
+})
+export class CourseListComponent {
+  courses = [
+    { courseId: 1, courseName: 'Node JS' },
+    { courseId: 2, courseName: 'Typescript' },
+    { courseId: 3, courseName: 'Angular' },
+    { courseId: 4, courseName: 'React JS' },
+  ];
+  @Input() cName: string ='';
+}
